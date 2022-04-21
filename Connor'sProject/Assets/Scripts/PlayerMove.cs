5649984c156ce98be22f71ee31c63032bb4efb45
Danny-Sammon <90838410+Danny-Sammon@users.noreply.github.com>
2022-04-21 18:10:08 +0100

    upload

--- Connor'sProject/Assets/Scripts/PlayerMove.cs
+++ Connor'sProject/Assets/Scripts/PlayerMove.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    private SpriteRenderer sprite;
-    private Animator anim;
+    public Animator anim;
+    private SpriteRenderer sp;
+    //private Scene scene;
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpGround;
+    
 
     private float dirX = 0f;
     [SerializeField] private float speed = 7f;
-    [SerializeField] private LayerMask jumpGround;
-    private BoxCollider2D coll;
-    private float Jumpforce = 4f;
-    public int points = 0;
-    private enum movement { idle, right, jump };
+    private float Jumpforce = 8f;
 
-    private void Start()
+    private enum movement { idle, right, jump, fall };
+    
+
+
+    void Start()
     {
-        Debug.Log("Tis Working");
+        //scene = SceneManager.GetActiveScene();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
-    }
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpGround);
+        sp = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
-
+    // Update is called once per frame
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
@@ -42,32 +43,38 @@ public class PlayerMove : MonoBehaviour
 
         Animations();
     }
-    private void Animations()
-        {
-            movement state;
-            if (dirX > 0f)
-            {
-                state = movement.right;
-                sprite.flipX = false;
-            }
-            else if (dirX < 0)
-            {
-                state = movement.right;
-                sprite.flipX = true;
-            }
-            else
-            {
-                state = movement.idle;
-            }
-            if (rb.velocity.y > 0.1f)
-            {
-                state = movement.jump;
-            }
-            else if (rb.velocity.y < -0.1f)
-            {
-                state = movement.idle;
-            }
 
-            anim.SetInteger("state", (int)state);
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpGround);
+    }
+
+    private void Animations()
+    {
+        movement state;
+        if (dirX > 0f)
+        {
+            state = movement.right;
+            sp.flipX = false;
         }
+        else if (dirX < 0)
+        {
+            state = movement.right;
+            sp.flipX = true;
+        }
+        else
+        {
+            state = movement.idle;
+        }
+        if (rb.velocity.y > 0.1f)
+        {
+            state = movement.jump;
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            state = movement.fall;
+        }
+         anim.SetInteger("state", (int) state);
+    }
+   
 }
